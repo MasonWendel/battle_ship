e12e4b71cb2e2dd7f4d6fdabc0ca6a26a0dd70b5
MasonWendel <masonwendel@outlook.com>
2021-12-16 15:41:46 -0600

    Finished background for now

--- battle_ship_game.py
+++ battle_ship_game.py
@@ -1,4 +1,10 @@
 # This is where evrything is imported, where the main game loop runs, and where events are checked for. 
 # Nothing yet to be placed.
 import arcade
-arcade.open_window(1000,1000,'drawing example')
+arcade.open_window(3000,1000,'drawing example')
+# This is a background color until we mess with sprites ->
+arcade.set_background_color(arcade.color.CHARCOAL)
+arcade.start_render()
+# drawing goes here
+arcade.finish_render()
+arcade.run()
--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-# Hi this is the test file.
-print("hello world")
-# this is a test 
+import arcade
+
+# Open up a window.
+# From the "arcade" library, use a function called "open_window"
+# Set the window title to "Drawing Example"
+# Set the dimensions (width and height)
+arcade.open_window(600, 600, "Drawing Example")
+
+# Set the background color
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+
+# Get ready to draw
+arcade.start_render()
+
+# (The drawing code will go here.)
+
+# Finish drawing
+arcade.finish_render()
+
+# Keep the window up until someone closes it.
+arcade.run()
