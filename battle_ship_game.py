10a4fb16b717b7a21afef403dcb9d791b244901c
Alex <96257041+BadCoderButStillBigBrain@users.noreply.github.com>
2021-12-16 12:44:08 -0600

    Update battle_ship_game.py

--- battle_ship_game.py
+++ battle_ship_game.py
@@ -1,3 +1,4 @@
 # This is where evrything is imported, where the main game loop runs, and where events are checked for. 
 # Nothing yet to be placed.
 import arcade
+arcade.open_window(1000,1000,'drawing example')
