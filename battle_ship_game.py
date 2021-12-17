71f28c7c7b0d32105c71b1a534d5de7ecc8c1a9d
MasonWendel <masonwendel@outlook.com>
2021-12-17 05:22:43 -0600

    Made grid lines

--- battle_ship_game.py
+++ battle_ship_game.py
@@ -1,13 +1,17 @@
 # This is where evrything is imported, where the main game loop runs, and where events are checked for. 
 # Nothing yet to be placed.
 import arcade
-arcade.open_window(2000,900,'drawing example')
+arcade.open_window(1900,900,'drawing example')
 # This is a background color until we mess with sprites ->
 arcade.set_background_color(arcade.color.CHARCOAL)
 arcade.start_render()
 # drawing goes here
 # seperator. needs adjustments
-arcade.draw_lrtb_rectangle_filled(850,1050, 900,0,arcade.csscolor.BLACK)
+for i in range(19):
+    arcade.draw_line((i * 100), 0,(i * 100), 900, arcade.csscolor.BLACK)
+for i in range(9):
+    arcade.draw_line(0, (i * 100),1900, (i * 100), arcade.csscolor.BLACK)
+arcade.draw_lrtb_rectangle_filled(900,1000, 900,0,arcade.csscolor.BLACK)
 # drawing ends here
 arcade.finish_render()
 arcade.run()
